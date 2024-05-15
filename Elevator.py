ebcf87ee786ff037b93679045647c3b7960da059
AharonLif <a0556771782@gmail.com>
2024-05-16 02:49:07 +0300

    building and Elevator

--- Elevator.py
+++ Elevator.py
@@ -1,93 +1,37 @@
 import pygame as pg
-import sys
 
-import floor
+# Initialize Pygame
+pg.init()
 
-#f = floor.p
-def p():
-    print(9)
-#print(f.pr())
+# Set up the display
+width, height = 800, 600
+screen = pg.display.set_mode((width, height))
+pg.display.set_caption("Building Background")
 
-# class ColorDraw:
-#     black = (0, 0, 0)
-#     red = (0, 0, 0)
-#     white = (255, 255, 255)
-class Building(pg.sprite.Sprite):
-    def __init__(self, x, y, width, height, floors):
-        super().__init__()
-        self.image = pg.Surface((width, height))
-        # self.image.fill(color)
-        self.rect = self.image.get_rect(topleft=(x, y))
-        self.walls = []
-        self.floors = floors
-        # Add black walls to the building
-        for floor in self.floors:
-            #new_floor = floor.Floor(floor)
-            #pg.draw.new_floor(self.image, (0, 0, 0), (x, y), (x, y), 2)
-            pass
-        self._add_walls()
+# Define colors
+BLACK = (0, 0, 0)
 
+# Create a surface for the black background
+background_surface = pg.Surface((width, height))
+background_surface.fill(BLACK)
 
-    def _add_walls(self):
-        top = self.rect
-        line_width = 4
-        #pg.draw.line(self.image, (0, 0, 0), top, line_width)
-class BuildingFloor:
-    def __init__(self, width, height, color, building_line):
-        self.width = width
-        self.height = height
-        self.color = color
-        self.buildings = []
-        self.building_line = building_line
+# Load building image
+building_image = pg.image.load("building_image.png")  # Replace "building_image.png" with your actual image file
 
-    def add_building(self, building):
-        self.buildings.append(building)
+# Blit building image onto the background
+building_rect = building_image.get_rect(center=(width // 2, height // 2))
+background_surface.blit(building_image, building_rect)
 
-    def draw(self, surface):
-        pg.draw.rect(surface, self.color, (0, surface.get_height() - self.height, self.width, self.height))
-        # for building in self.buildings:
-        #     building.draw(surface)
+# Main loop
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
 
+    screen.blit(background_surface, (0, 0))  # Blit the background onto the screen
 
-def main():
-    p()
-    pg.init()
+    pg.display.flip()
 
-    # Set up the display
-    width, height = 800, 600
-    screen = pg.display.set_mode((width, height))
-    pg.display.set_caption("Single Building Floor")
-
-    # Define the building line
-    building_line = [(100, 300), (300, 400), (600, 200)]
-
-    # Create a building floor object
-    building_floor = BuildingFloor(width, 500, (100, 100, 100), building_line)
-
-    # Generate buildings based on the building line
-    for i in range(len(building_line) - 1):
-        x1, y1 = building_line[i]
-        x2, y2 = building_line[i + 1]
-        building_width = x2 - x1
-        building_height = y1
-        building = Building(x1, 0, building_width, building_height, (200, 200, 200))
-        building_floor.add_building(building)
-
-    running = True
-    while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-
-        screen.fill((255, 255, 255))  # Fill the screen with white color
-
-        # Draw the building floor
-        building_floor.draw(screen)
-
-        pg.display.flip()
-
-    pg.quit()
-    sys.exit()
-
-if __name__ == "__main__":
-    main()
+# Quit Pygame
+pg.quit()
--- floor.py
+++ floor.py
@@ -1,30 +1,35 @@
 import pygame as pg
-import sys
 
 
-class p:
-    def __init__(self):
-        """_summary_
+# class absimg(pg.sprite.Sprite):
+#     def __init__(self, image_path):
+#         super()
+#         pg.image.load(image_path)# TODO: check if can to do abstract class with image load instead in floor and elevator classes
+class BlackLine(pg.sprite.Sprite):
+    BLACK = (0, 0, 0)
 
-        Returns:
-            _type_: _description_
-        """
-    def pr():
-        print ("llllllllll")
-class Floor(pg.sprite.Sprite):
-    def __init__(self, floor_number):
+    def __init__(self, start, end, width=4):
         super().__init__()
-
-        self.width = 10
-        self.height = 4
-        self.image = pg.Surface((800, 600))
+        self.image = pg.Surface((abs(end[0] - start[0]), abs(end[1] - start[1])))
+        self.image.fill(self.BLACK)
+        self.rect = self.image.get_rect(bottomleft=start)
+class Floor(pg.sprite.Sprite):
+    width, height = 150, 80
+    def __init__(self, floor_number, bottomleft):
+        super().__init__()
+        self.image = pg.image.load("help_files/floor_image.png")
+        self.image = pg.transform.scale(self.image, (self.width, self.height))
+        self.rect = self.image.get_rect(bottomleft=bottomleft)
         self.floor_number = floor_number
-        # self.buildings = []
-        # self.surface.fill((255, 255, 255))
-        self.rect = self.image.get_rect()
+        height_beginning_buildings = 50
+        #The height of the floor is the height where the building starts, 
+        #plus the number of floors times the height of each floor plus the height of the dividing line as the number of lines between the floors
+        self.rect_y = height_beginning_buildings + self.floor_number*self.height 
 
-    def convert_to_right_size(width, height, image_path):
-        image = pg.image.load(image_path).convert_alpha()
-        image = pg.transform.scale(image, (width, height))
-        return image
-
+class Elevator(pg.sprite.Sprite):
+    width, height = 80, 80
+    def __init__(self, bottomleft):
+        super().__init__()
+        self.image = pg.image.load("help_files/elv.png")
+        self.image = pg.transform.scale(self.image, (self.width, self.height))
+        self.rect = self.image.get_rect(bottomleft=bottomleft)
