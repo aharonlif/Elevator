f91915e4b5d92997cf4ee97591c3cc4ec02bdad6
AharonLif <a0556771782@gmail.com>
2024-05-16 15:54:10 +0300

    Button class

--- floor.py
+++ floor.py
@@ -1,35 +1,37 @@
 import pygame as pg
 
+class Button(pg.sprite.Sprite):
+    size = (50, 50)
+    b_color = (255, 255, 255)
+    text_color = (0, 0, 0)
 
-# class absimg(pg.sprite.Sprite):
-#     def __init__(self, image_path):
-#         super()
-#         pg.image.load(image_path)# TODO: check if can to do abstract class with image load instead in floor and elevator classes
-class BlackLine(pg.sprite.Sprite):
-    BLACK = (0, 0, 0)
-
-    def __init__(self, start, end, width=4):
+    def __init__(self, number):
         super().__init__()
-        self.image = pg.Surface((abs(end[0] - start[0]), abs(end[1] - start[1])))
-        self.image.fill(self.BLACK)
-        self.rect = self.image.get_rect(bottomleft=start)
+        self.number = number
+        self.font = pg.font.SysFont(None, 30)
+        self.image = self.create_button_image()
+        self.rect = self.image.get_rect()
+
+    def create_button_image(self):
+        button_surface = pg.Surface(self.size)
+        button_surface.fill(self.b_color)
+        text_surface = self.font.render(str(self.number), True, self.text_color)
+        text_rect = text_surface.get_rect(center=button_surface.get_rect().center)
+        button_surface.blit(text_surface, text_rect)
+        return button_surface
+
 class Floor(pg.sprite.Sprite):
     width, height = 150, 80
     def __init__(self, floor_number, bottomleft):
         super().__init__()
+
         self.image = pg.image.load("help_files/floor_image.png")
         self.image = pg.transform.scale(self.image, (self.width, self.height))
         self.rect = self.image.get_rect(bottomleft=bottomleft)
         self.floor_number = floor_number
-        height_beginning_buildings = 50
-        #The height of the floor is the height where the building starts, 
-        #plus the number of floors times the height of each floor plus the height of the dividing line as the number of lines between the floors
-        self.rect_y = height_beginning_buildings + self.floor_number*self.height 
+        self.draw_button()
+      
+    def draw_button(self):
+        button = Button(self.floor_number)
+        self.image.blit(button.image, button.rect.center)
 
-class Elevator(pg.sprite.Sprite):
-    width, height = 80, 80
-    def __init__(self, bottomleft):
-        super().__init__()
-        self.image = pg.image.load("help_files/elv.png")
-        self.image = pg.transform.scale(self.image, (self.width, self.height))
-        self.rect = self.image.get_rect(bottomleft=bottomleft)
