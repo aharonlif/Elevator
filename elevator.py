775bf4bf8a339640726b8422b74a7af3cb8d4ac3
AharonLif <a0556771782@gmail.com>
2024-05-19 16:31:42 +0300

    Button class

--- elevator.py
+++ elevator.py
@@ -1,16 +1,52 @@
 import pygame as pg
-
-
+import time
+    
 class Elevator(pg.sprite.Sprite):
     width, height = 80, 80
     def __init__(self, bottomleft):
         super().__init__()
         self.image = pg.image.load("help_files/elv.png")
         self.image = pg.transform.scale(self.image, (self.width, self.height))
+        self.location = bottomleft
         self.rect = self.image.get_rect(bottomleft=bottomleft)
+        self.floor = 0
 
     def move(self, floor):
-        pass
-    
+        #TODO: move
+        while self.floor < floor:
+
+            self.rect.y += time.time()   
+
+
+# import time
+
+# class Elevator(pg.sprite.Sprite):
+#     def __init__(self, floors):
+#         super().__init__()
+#         self.current_floor = 0  # Assume elevator starts at floor 0
+#         self.floors = floors
+
+#     def move_to_floor(self, target_floor):
+#         # Move the elevator to the target floor
+#         if target_floor > self.current_floor:
+#             direction = 1  # Move up
+#         elif target_floor < self.current_floor:
+#             direction = -1  # Move down
+#         else:
+#             return  # Already at the target floor
+
+#         # Move the elevator to each floor sequentially with a delay
+#         while self.current_floor != target_floor:
+#             self.current_floor += direction
+#             print(f"Moving elevator to floor {self.current_floor}")
+#             time.sleep(0.5)  # Delay for half a second between each floor
+
+#         print("Elevator reached destination floor.")
+
+# # Usage example:
+# elevator = Elevator(10)  # Assume 10 floors
+# target_floor = 5  # Target floor to move the elevator to
+# elevator.move_to_floor(target_floor)
+
 
 
--- main.py
+++ main.py
@@ -1,46 +1,16 @@
 import pygame as pg
 
-import floor as floor_file
-# import Elevator
-
+import building
 SCREEN_SIZE = 1200, 900
 
-class Building(pg.sprite.Group):
-    difference_building = floor_file.Floor.width*2 + floor_file.Elevator.width
-    
-    def __init__(self, floors, building_number=0):
-        super().__init__()
-        self.floors = floors
-        self.building_number = building_number
-        self.rect_x = 40 + building_number * (floor_file.Floor.width + 40) # Distance from the beginning of the screen + distance from the last building
-        self._build_building_floors(floors=floors)
-    
+  
 
-    def _build_building_floors(self, floors):
-        for i in range(floors):
-            _, position_y = SCREEN_SIZE
-            position_y -= 50 # difference from the floor
-            position_y -= i * (floor_file.Floor.height + 4)#line width. need to add hight above building.
-            new_floor = f"floor_{i}"
-            locals()[new_floor] = floor_file.Floor(i, bottomleft=(200, position_y))
-            self.add(locals()[new_floor])
+pg.init()
 
-    def boundary_line(self, start, end, y_position):
-        d = floor_file.Elevator()
-
-
-
-    # def _draw_walls(self):
-    #     top = self.rect
-    #     line_width = 4
-    #     #pg.draw.line(self.image, (0, 0, 0), top, line_width)
-
-
-grp = Building(9)
+grp = building.Building(9, 1)
 
 
 def main():
-    pg.init()
 
     width, height = SCREEN_SIZE
     screen = pg.display.set_mode((width, height))
@@ -50,7 +20,7 @@ def main():
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                running = False
+                running = False 
 
         screen.fill((255, 255, 255)) 
         grp.draw(screen)
